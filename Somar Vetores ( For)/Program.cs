1dcd0542e7a496e87c35f310f4b89d5683dff0c5
washington1313 <123412352+washington1313@users.noreply.github.com>
2023-04-13 17:46:52 -0300

    Add files via upload

---
+++ Somar Vetores ( For)/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Somar_Vetores___For_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeroEntrada = new int[5];
+            int soma = 0;
+
+            for (int i = 0; i < numeroEntrada.Length; i++)
+            {
+                Console.WriteLine("Digite os números a serem somados: ");
+                numeroEntrada[i] = int.Parse(Console.ReadLine());
+
+                Console.Clear();
+            }
+
+            for (int i = 0; i < numeroEntrada.Length; i++)
+            {
+                soma += numeroEntrada[i];
+                Console.Clear();
+
+                Console.WriteLine("A soma dos números é: " + soma);
+            }
+            for (int i = 0; i < numeroEntrada.Length; i++)
+            {
+                Console.WriteLine("Os números somados são: " + numeroEntrada[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
